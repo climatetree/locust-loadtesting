ca2ad3e193d4c27247bc8d5f88e4cbfbfeb64213
Wanjia Tang <tang.wan@husky.neu.edu>
2020-04-03 13:03:01 -0700

    save

--- locustfile.py
+++ locustfile.py
@@ -16,9 +16,40 @@ class UserBehavior(TaskSet):
     def index(self):
         self.client.get("/stories")
 
-    # @task(1)
-    # def profile(self):
-    #     self.client.get("/profile")
+    @task(2)
+    def login(self):
+        self.client.post("/login",  {"username":"shwetamandavgane123","email":"mandavgane@gmail.com"})
+
+
+    @task(3)
+    def profile(self):
+        # self.client.get("/profile")
+        self.client.get("/stories")
+        # stories microservice
+        self.client.get("https://backend-mongo-stories.azurewebsites.net/stories?page=1&limit=100")
+        # need a new story id
+        #self.client.get("https://backend-mongo-stories.azurewebsites.net/stories/story/5e3899c5463a27ee9a334781")
+        self.client.get("https://backend-mongo-stories.azurewebsites.net/stories/place/1")
+        self.client.get("https://backend-mongo-stories.azurewebsites.net/stories/title/china?page=1&limit=1")
+        self.client.get("https://backend-mongo-stories.azurewebsites.net/stories/topStories/3")
+        self.client.get("https://backend-mongo-stories.azurewebsites.net/stories/comment")
+
+        # places microservice
+        self.client.get("https://places-postgres2.azurewebsites.net/api/places/:searchTerm")
+        self.client.get("https://places-postgres2.azurewebsites.net/api/places/3233/similar")
+        self.client.get("https://places-postgres2.azurewebsites.net/api/places/3233/similar/advanced?populationStart=80&populationEnd=110&carbonStart=90&carbonEnd=200&perCapCarbonStart=70&perCapCarbonEnd=130&popDensityStart=50&popDensityEnd=150")
+        self.client.get("https://places-postgres2.azurewebsites.net/api/places/nearest?latitude=42&longitude=-72")
+        
+        # user microservice
+        self.client.get("https://user-microservice-demo.herokuapp.com/user")
+        # how to test login
+        # self.client.get("https://user-microservice-demo.herokuapp.com/login")
+        self.client.get("https://usermicroservice-climatetree.azurewebsites.net/user/searchname?name=shwetamandavgane123")
+        self.client.get("https://usermicroservice-climatetree.azurewebsites.net/user/searchname?name=shwetamandavgane123")
+        self.client.get("https://usermicroservice-climatetree.azurewebsites.net/user/searchrole?roleid=3")
+        
+        # self.client.put("https://usermicroservice-climatetree.azurewebsites.net/user/")
+        # self.client.post("https://usermicroservice-climatetree.azurewebsites.net/user/addUser")
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
