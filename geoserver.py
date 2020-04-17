129a5a57bc5ee9b59007514f4c9471b4099546a6
patelvp <patel.visha@husky.neu.edu>
2020-04-17 17:26:33 -0400

    Update geoserver error

--- geoserver.py
+++ geoserver.py
@@ -3,7 +3,7 @@ from locust import HttpLocust, TaskSet, task
 class UserBehavior(TaskSet):
 	@task(1)
 	def places_test(self):
-		self.client.get("https://climatetree-geoserver.azurewebsites.net/geoserver/wfs?service=wfs&version=2.0.0&request=GetFeature&typeNames=ClimateTree:Similar_Places&outputFormat=application/json&viewparams=TYPE_ID_1:1;TYPE_ID_2:2;TYPE_ID_3:3;TYPE_ID_4:4")
+		self.client.get("https://climatetree-api-gateway.azurewebsites.net/geoserver/wfs?service=wfs&version=2.0.0&request=GetFeature&typeNames=ClimateTree:Similar_Places&outputFormat=application/json&viewparams=TYPE_ID_2:2;POPULATION_LOW:4000;POPULATION_HIGH:500000")
 
 class WebsiteUser(HttpLocust):
 	task_set = UserBehavior
